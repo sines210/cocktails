ff6df1cb0dab3e5bc82dee034f3d936923ad1808
root <root@debian2.debian2>
2022-01-17 11:40:26 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
  {  path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-    
+
+  {path: 'folder',
+  loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
   {path: 'folder/:id',
    redirectTo: 'folder2/:id',
    pathMatch: 'full'},
